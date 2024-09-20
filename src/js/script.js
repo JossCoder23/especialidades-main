ba5ab3ee67016a276b4611133337422fb320e3b1
Jose Pardo <jose.pardo@upch.pe>
2024-09-20 17:53:15 -0500

    update05532009

--- src/js/script.js
+++ src/js/script.js
@@ -17,6 +17,55 @@ window.addEventListener("load", () => {
         breakpoints: {}
     });
 
+    let receptor1 = document.querySelector('.LeftBottomItemTop1');
+    let blockTop1 = document.querySelector('.LeftBottomItemBottom1');
+    let receptor2 = document.querySelector('.LeftBottomItemTop2');
+    let blockTop2 = document.querySelector('.LeftBottomItemBottom2');
+    let receptor3 = document.querySelector('.LeftBottomItemTop3');
+    let blockTop3 = document.querySelector('.LeftBottomItemBottom3');
+    let receptor4 = document.querySelector('.LeftBottomItemTop4');
+    let blockTop4 = document.querySelector('.LeftBottomItemBottom4');
+    let receptor5 = document.querySelector('.LeftBottomItemTop5');
+    let blockTop5 = document.querySelector('.LeftBottomItemBottom5');
+    let receptor6 = document.querySelector('.LeftBottomItemTop6');
+    let blockTop6 = document.querySelector('.LeftBottomItemBottom6');
+    let receptor7 = document.querySelector('.LeftBottomItemTop7');
+    let blockTop7 = document.querySelector('.LeftBottomItemBottom7');
+    let receptor8 = document.querySelector('.LeftBottomItemTop8');
+    let blockTop8 = document.querySelector('.LeftBottomItemBottom8');
+
+    receptor1.addEventListener("click", () => {
+        blockTop1.classList.toggle('other');
+    })
+
+    receptor2.addEventListener("click", () => {
+        blockTop2.classList.toggle('other');
+    })
+
+    receptor3.addEventListener("click", () => {
+        blockTop3.classList.toggle('other');
+    })
+
+    receptor4.addEventListener("click", () => {
+        blockTop4.classList.toggle('other');
+    })
+
+    receptor5.addEventListener("click", () => {
+        blockTop5.classList.toggle('other');
+    })
+
+    receptor6.addEventListener("click", () => {
+        blockTop6.classList.toggle('other');
+    })
+
+    receptor7.addEventListener("click", () => {
+        blockTop7.classList.toggle('other');
+    })
+
+    receptor8.addEventListener("click", () => {
+        blockTop8.classList.toggle('other');
+    })
+
     // var slider3 = new Swiper(".mySwiper3", {
     //     navigation: {
     //         nextEl: ".swiper-button-next3",
